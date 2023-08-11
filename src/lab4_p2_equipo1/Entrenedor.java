2182fa9c1a5b098fabf48774ea20bc1af6a8c434
Haziel Pavon <hazielpavon93@gmail.com>
2023-08-11 14:00:51 -0600

    Iniciando clases y main

---
+++ src/lab4_p2_equipo1/Entrenedor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4_p2_equipo1;
+
+/**
+ *
+ * @author hapav
+ */
+public class Entrenedor {
+    
+}
