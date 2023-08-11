900b0ee012e96b22a4f6e193930327442860a23c
Haziel Pavon <hazielpavon93@gmail.com>
2023-08-11 13:39:14 -0600

    Proyecto

---
+++ src/lab4_p2_equipo1/Lab4_P2_Equipo1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab4_p2_equipo1;
+
+/**
+ *
+ * @author hapav
+ */
+public class Lab4_P2_Equipo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
