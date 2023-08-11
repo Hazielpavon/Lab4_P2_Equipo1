a42102b0c160ec55b17f02338025f92c7c76b7e2
Haziel Pavon <hazielpavon93@gmail.com>
2023-08-11 14:36:41 -0600

    Update

---
+++ src/lab4_p2_equipo1/Entrenador.java
@@ -0,0 +1,54 @@
+
+package lab4_p2_equipo1;
+
+
+public class Entrenador {
+    
+ private String Nombre; 
+ private int edad; 
+ private double dinero; 
+
+    public Entrenador() {
+    }
+
+    public Entrenador(String Nombre, int edad, double dinero) {
+        this.Nombre = Nombre;
+        this.edad = edad;
+        this.dinero = dinero;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(double dinero) {
+        this.dinero = dinero;
+    }
+
+    @Override
+    public String toString() {
+        return " Entrenedor: " + "Nombre = " + Nombre + " , edad = " + edad + " , dinero = "  + dinero;
+    }
+    
+    
+    
+    
+    
+   
+}
--- src/lab4_p2_equipo1/Lab4_P2_Equipo1.java
+++ src/lab4_p2_equipo1/Lab4_P2_Equipo1.java
@@ -1,20 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab4_p2_equipo1;
 
-/**
- *
- * @author hapav
- */
+import java.util.Scanner;
+
 public class Lab4_P2_Equipo1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+
+        boolean seguir = true;
+
+        while (seguir) {
+
+            System.out.println("1. Registrar Entrenador");
+            System.out.println("2. Battle Factory");
+            System.out.println("3. Capturar/Entrnar");
+            System.out.println("4. Salir");
+
+            int opcion = leer.nextInt();
+
+            switch (opcion) {
+
+                case 1:
+                    System.out.println("Ingrese el nombre del entrnador");
+                    String nombree;
+
+                    nombree = leer.next();
+
+                    System.out.println("Ingrese la edad");
+                    int edad = leer.nextInt();
+
+                    System.out.println("Ingrese la cantidad de dinero en la cuenta");
+                    double dineroencuenta = leer.nextDouble();
+
+                    Entrenador a = new Entrenador(nombree, edad, dineroencuenta);
+                    
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    seguir = false;
+                    break;
+
+                default:
+
+                    System.out.println("Ingrese un dato correcto");
+            }
+        }
+
     }
-    
+
 }
